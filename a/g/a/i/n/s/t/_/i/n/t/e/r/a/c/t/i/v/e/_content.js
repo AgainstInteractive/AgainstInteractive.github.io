69ec3114a9773347e7a102c3391c57be1b6eba22
Against Interactive <zihmanisoskars05@gmail.com>
2026-07-20 15:21:34 +0300

    Create _content.js

---
+++ a/g/a/i/n/s/t/_/i/n/t/e/r/a/c/t/i/v/e/_content.js
@@ -0,0 +1,45 @@
+var a = 1;
+a = a + 1; if (a === 2){
+  console.log('check')
+}
+else if (a === 3) {
+    const button = document.getElementById('buton');
+    
+    if (button) {
+        button.addEventListener('click', function showSecret() {
+            console.log("button enabled");
+        });
+    }
+}
+var b = 3;
+a + b;
+// 1 + 3 = 4 
+ if (a + b === 4){
+  const z =  document.getElementById('buton');
+
+  if (buton) {
+    z.addEventListener('click', function showSecret(){
+      console.clear()
+      var f = 33; 
+      f = f * l;
+      l = 2;
+      // 33 * 2 = 66
+     if ( f === 33){
+       const cf2 = document.createElement("p")
+       cf2.style.color = 'white';
+       cf2.textContent = "."
+       document.body.appendChild(cf2);
+     } 
+     else if ( f === 66){
+       cf2.style.color = 'black';
+       cf2.textContent = "."
+       document.body.appendChild(cf2);
+     }
+     else if (f*l === 66){ 
+       cf2.style.color = 'white';
+       cf2.textContent = "."
+       document.body.appendChild(cf2);
+     }
+    });
+  }
+ }
