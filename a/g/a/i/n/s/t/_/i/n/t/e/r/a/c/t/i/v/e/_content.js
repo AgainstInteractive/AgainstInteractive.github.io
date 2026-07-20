b6f1bc7f716885cfa757e17f8fc73f08192db2ec
Against Interactive <zihmanisoskars05@gmail.com>
2026-07-20 15:26:22 +0300

    Update _content.js

--- a/g/a/i/n/s/t/_/i/n/t/e/r/a/c/t/i/v/e/_content.js
+++ a/g/a/i/n/s/t/_/i/n/t/e/r/a/c/t/i/v/e/_content.js
@@ -1,45 +1,48 @@
 var a = 1;
-a = a + 1; if (a === 2){
-  console.log('check')
-}
-else if (a === 3) {
+a = a + 1; 
+
+if (a === 2) {
+    console.log('check'); // This runs
+} else if (a === 3) {
     const button = document.getElementById('buton');
-    
     if (button) {
         button.addEventListener('click', function showSecret() {
             console.log("button enabled");
         });
     }
 }
-var b = 3;
-a + b;
-// 1 + 3 = 4 
- if (a + b === 4){
-  const z =  document.getElementById('buton');
 
-  if (buton) {
-    z.addEventListener('click', function showSecret(){
-      console.clear()
-      var f = 33; 
-      f = f * l;
-      l = 2;
-      // 33 * 2 = 66
-     if ( f === 33){
-       const cf2 = document.createElement("p")
-       cf2.style.color = 'white';
-       cf2.textContent = "."
-       document.body.appendChild(cf2);
-     } 
-     else if ( f === 66){
-       cf2.style.color = 'black';
-       cf2.textContent = "."
-       document.body.appendChild(cf2);
-     }
-     else if (f*l === 66){ 
-       cf2.style.color = 'white';
-       cf2.textContent = "."
-       document.body.appendChild(cf2);
-     }
-    });
-  }
- }
+var b = 3;
+
+if (a + b === 4) {
+    const z = document.getElementById('buton');
+
+    if (z) { 
+        z.addEventListener('click', function showSecret() {
+            console.clear();
+            
+            var f = 33; 
+            var l = 2;
+            f = f * l; 
+            
+          
+            const cf2 = document.createElement("p"); 
+            
+            if (f === 33) {
+                cf2.style.color = 'white';
+                cf2.textContent = ".";
+                document.body.appendChild(cf2);
+            } 
+            else if (f === 66) {
+                cf2.style.color = 'black';
+                cf2.textContent = ".";
+                document.body.appendChild(cf2);
+            }
+            else if (f * l === 132) { 
+                cf2.style.color = 'white';
+                cf2.textContent = ".";
+                document.body.appendChild(cf2);
+            }
+        });
+    }
+}
